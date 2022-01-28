b1b9e6ba5c236479ab63ea9f9ea1d1774171ac91
Mark-raven <markgerald360@gmail.com>
2022-01-29 00:35:17 +0530

    Enter

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyBBJYbiEkCQatGTEx8HFPpy77d-JcsqGz4",
+  authDomain: "authorization-91159.firebaseapp.com",
+  projectId: "authorization-91159",
+  storageBucket: "authorization-91159.appspot.com",
+  messagingSenderId: "508141701183",
+  appId: "1:508141701183:web:1965f259a6dc182482508a",
+  measurementId: "G-6EWWN9EXP2",
 };
 
 class Firebase {
